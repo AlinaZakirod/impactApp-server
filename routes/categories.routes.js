7f885c0f1ee84e990ba9bb7b30e4c6919e3c7e6b
AlinaZakirod <gabie1989@gmail.com>
2019-10-18 13:59:57 -0400

    category create route

--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -4,19 +4,17 @@ const router = express.Router();
 
 const Category = require("../models/Category");
 
-router.get("/category/new", (req, res, next) => {
-  Category.find()
-    .then(allCategories => req.body({ allCategories }))
-    .catch(err => console.log("Error while creating a new plan", err));
-});
+// router.post("/category/new", (req, res, next) => {
+//   console.log(req.body);
+// });
 
 router.post("/category/create", (req, res, next) => {
-  let newTitle = req.body.title;
-  let newDescription = req.body.description;
+  // let newTitle = req.body.title;
+  // let newDescription = req.body.description;
 
   Category.create({
-    title: newTitle,
-    description: newDescription,
+    title: req.body.title,
+    description: req.body.description,
     actions: []
   })
     .then(newCategory => {
@@ -25,11 +23,23 @@ router.post("/category/create", (req, res, next) => {
     .catch(err => console.log("error while creating a new category", err));
 });
 
-// R display all categories in the home page
-// router.get("/", (req, res, next) => {
-//   Category.find()
-//     .then(categoriesFromDb => console.log({categoriesFromDb}))
-//     .catch(err => console.log("Error displaying all categories", err));
-// });
+// Read 1  display all categories in the home page
+router.post("/", (req, res, next) => {
+  Category.find()
+    .then(allCategories => res.json({ allCategories }))
+    .catch(err => console.log("Error displaying all categories", err));
+});
+
+// Read 2 display details of a particular category
+router.post("/category/:theId", (req, res, next) => {
+  Category.findById(req.params.theId);
+});
+
+//Delete category route:
+router.post("/category/:theId/delete", (req, res, next) => {
+  Category.findByIdAndDelete(req.params.theId)
+    .then(() => console.log("deleted"))
+    .catch(err => console.log("error while delteing the category: ", err));
+});
 
 module.exports = router;
