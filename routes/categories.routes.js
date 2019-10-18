95f712a19896910dfe28b527935df12b4390c1b6
AlinaZakirod <gabie1989@gmail.com>
2019-10-17 22:03:58 -0400

    categories model

--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,7 +1,35 @@
 const express = require("express");
 
-const authRouter = express.Router();
+const router = express.Router();
 
 const Category = require("../models/Category");
 
-module.exports = authRouter;
+router.get("/category/new", (req, res, next) => {
+  Category.find()
+    .then(allCategories => req.body({ allCategories }))
+    .catch(err => console.log("Error while creating a new plan", err));
+});
+
+router.post("/category/create", (req, res, next) => {
+  let newTitle = req.body.title;
+  let newDescription = req.body.description;
+
+  Category.create({
+    title: newTitle,
+    description: newDescription,
+    actions: []
+  })
+    .then(newCategory => {
+      res.json({ newCategory });
+    })
+    .catch(err => console.log("error while creating a new category", err));
+});
+
+// R display all categories in the home page
+// router.get("/", (req, res, next) => {
+//   Category.find()
+//     .then(categoriesFromDb => console.log({categoriesFromDb}))
+//     .catch(err => console.log("Error displaying all categories", err));
+// });
+
+module.exports = router;
